46cec874a13c29678040d15553799f4d25629ce2
Prajwal Dhage <psdhage1@gmail.com>
2025-02-07 09:34:04 +0530

    added backend v2: includes weather, milk production and egg production

---
+++ backend/modules/variables.py
@@ -0,0 +1,8 @@
+city = "Pune"
+weather_api = "AJXv10q0BwzUyyspBTqDsb7h6CK8GBUG"
+api_url = "http://dataservice.accuweather.com"
+
+# Database Credentials
+host_name = "localhost"
+user_name = "root"
+user_password = "admin"
