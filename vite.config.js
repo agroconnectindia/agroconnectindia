e65731733fb3c9abe1492c5810d43737995c6466
Prajwal Dhage <psdhage1@gmail.com>
2025-03-20 15:54:04 +0530

    updated base

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/agroconnectindia/',  // Replace with your GitHub repo name
+  base: '/',  // Replace with your GitHub repo name
 });
